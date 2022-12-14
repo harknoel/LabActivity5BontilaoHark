237706edd15d262be2ba3914fee1a65f5a204791
Hark Noel A. Bontilao <bontilao.harknoel@gmail.com>
2022-12-15 01:03:21 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,9 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
@@ -10,7 +13,43 @@ public class SimpleCalcGUI extends JFrame {
     private JTextField tfNumber2;
     private JTextField lblResult;
 
-    public static void main(String[] args) {
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                compute();
+            }
+        });
+    }
 
+    void compute() {
+        Double operand1 = Double.parseDouble(tfNumber1.getText());
+        Double operand2 = Double.parseDouble(tfNumber2.getText());
+        String operator = (String) cbOperations.getSelectedItem();
+        double result;
+        switch (Objects.requireNonNull(operator)) {
+            case "+":
+                result = operand1 + operand2;
+                break;
+            case "-":
+                result = operand1 - operand2;
+                break;
+            case "*":
+                result = operand1 * operand2;
+                break;
+            default:
+                result = operand1 / operand2;
+        }
+        lblResult.setText(String.format("%.2f", result));
+    }
+
+    public static void main(String[] args) {
+        SimpleCalcGUI SimpleCalcGUI = new SimpleCalcGUI();
+        SimpleCalcGUI.setTitle("Simple Calculator");
+        SimpleCalcGUI.setLocationRelativeTo(null);
+        SimpleCalcGUI.setContentPane(SimpleCalcGUI.panel1);
+        SimpleCalcGUI.setSize(560, 250);
+        SimpleCalcGUI.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        SimpleCalcGUI.setVisible(true);
     }
 }
