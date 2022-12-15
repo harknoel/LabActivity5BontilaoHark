232bb2123396eeab9c8bc3e0a00cae41c0a96e1e
Hark Noel A. Bontilao <bontilao.harknoel@gmail.com>
2022-12-15 10:09:09 +0800

    Add exception handling to SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -23,26 +23,46 @@ public class SimpleCalcGUI extends JFrame {
     }
 
     void compute() {
-        Double operand1 = Double.parseDouble(tfNumber1.getText());
-        Double operand2 = Double.parseDouble(tfNumber2.getText());
-        String operator = (String) cbOperations.getSelectedItem();
-        double result;
-        switch (Objects.requireNonNull(operator)) {
-            case "+":
-                result = operand1 + operand2;
-                break;
-            case "-":
-                result = operand1 - operand2;
-                break;
-            case "*":
-                result = operand1 * operand2;
-                break;
-            default:
-                result = operand1 / operand2;
+        try {
+            Double operand1 = Double.parseDouble(tfNumber1.getText());
+            Double operand2 = Double.parseDouble(tfNumber2.getText());
+            String operator = (String) cbOperations.getSelectedItem();
+            Double result;
+            switch (Objects.requireNonNull(operator)) {
+                case "+":
+                    result = operand1 + operand2;
+                    break;
+                case "-":
+                    result = operand1 - operand2;
+                    break;
+                case "*":
+                    result = operand1 * operand2;
+                    break;
+                default:
+                    result = operand1 / operand2;
+                    if(result.isInfinite() || result.isNaN()) {
+                        throw new ArithmeticException();
+                    }
+            }
+            if(result == -0.00) {
+                result = (double) 0;
+            }
+            lblResult.setText(String.format("%.2f", result));
+        } catch(ArithmeticException e) {
+            JOptionPane.showMessageDialog(panel1, "Cannot divide by zero", "Error", JOptionPane.ERROR_MESSAGE);
+            clear();
+            System.out.println(e.getMessage());
+        } catch(NumberFormatException e) {
+            JOptionPane.showMessageDialog(panel1, "Input must be a number", "Error", JOptionPane.ERROR_MESSAGE);
+            clear();
         }
-        lblResult.setText(String.format("%.2f", result));
+
     }
 
+    void clear() {
+        tfNumber1.setText("");
+        tfNumber2.setText("");
+    }
     public static void main(String[] args) {
         SimpleCalcGUI SimpleCalcGUI = new SimpleCalcGUI();
         SimpleCalcGUI.setTitle("Simple Calculator");
